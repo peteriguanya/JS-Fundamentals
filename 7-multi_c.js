6ce93bab655a10950f9d24a0f9d4c39f94e43ba0
peteriguanya <iguanyapeter7@gmail.com>
2025-11-08 18:37:54 +0300

    Fix 7-multi_c.js to pass all checks

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,8 +1,6 @@
 const x = parseInt(process.argv[2]);
 
-if (isNaN(x) || x <= 0) {
-  console.log("Missing number of occurrences");
-} else {
+if (x > 0) {
   let i = 0;
   while (i < x) {
     console.log("C is fun");
